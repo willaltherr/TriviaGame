a127ab6f7c7c2ca6887ece0685ea8f66e5adfc06
Wills Grave <will@keydigitalconsulting.com>
2019-09-06 23:40:06 -0400

    Answer was not appearing in console log, had find that my correct answer was in an array rather than a string. Changed the correct answer to a string and now the correct answer can be selected.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,31 +2,31 @@ var quizQuestions = [
   {
     question: "What is the hottest planet in our Solar System?",
     choices: ["Earth", "Venus", "Mars", "Mercury"],
-    correctAnswer: ["Venus"]
+    correctAnswer: "Venus"
   },
 
   {
     question: "What year will Halleys Comet pass Earth next?",
     choices: ["2041", "2051", "2061", "2071"],
-    correctAnswer: ["2061"]
+    correctAnswer: "2061"
   },
 
   {
     question: "How much does a full NASA space suit cost?",
     choices: ["$1 Million", "$4 Million", "$8 Million", "$12 Million"],
-    correctAnswer: ["$12 Million"]
+    correctAnswer: "$12 Million"
   },
 
   {
     question: "How long is one day on Venus compared to Earth?",
     choices: ["78 Days", "144 Days", "198 Days", "243 Days"],
-    correctAnswer: ["243 Days"]
+    correctAnswer: "243 Days"
   },
 
   {
     question: "How fast do Neutron Stars spin per second?",
     choices: ["600", "700", "800", "900"],
-    correctAnswer: ["243 Days"]
+    correctAnswer: "600"
   }
 ]
 
@@ -97,18 +97,17 @@ function loadChoices(choices) {
 // Go to next question regardless of answer selection
 
 $(document).on('click', '.choice', function() {
-  const selectedAnswer = $(this).attr('data-answer');
-  const correctAnswer = quizQuestions[currentQuestion].correctAnswer;
+  var selectedAnswer = $(this).attr('data-answer');
+  var correctAnswer = quizQuestions[currentQuestion].correctAnswer;
 
-  if (correctAnswer === selectedAnswer) {
+  if (correctAnswer === selectedAnswer) {  
       correct++;
-      console.log('Winsss!!!!');
-      
-  } else {
+      console.log('wins');
+  } 
+  else {
       incorrect++;
-      console.log('Lost!!!!');
-      
-  }
+      console.log('loss');
+    }  
 });
 
 loadQuestion();
